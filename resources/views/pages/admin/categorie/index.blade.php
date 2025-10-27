2f3ca55f8874baaffd1c17dbf5a920b11adb3c2c
Ikhlasul Amal Mawardi <147895803+ikhlas-ct@users.noreply.github.com>
2025-10-27 14:15:10 +0700

    Develop (#7)
    * feat: crud controller manajer
    
    * Isi view kosong dengan layout dasar dan perbarui fungsi controller

---
+++ resources/views/pages/admin/categorie/index.blade.php
@@ -0,0 +1,14 @@
+{{-- ==================== PAGE TITLE ==================== --}}
+@section('title','Data Dategorie')
+
+{{-- ==================== STYLES ==================== --}}
+@section('styles')
+@endsection
+
+{{-- ==================== CONTENT ==================== --}}
+@section('content')
+@endsection
+
+{{-- ==================== SCRIPTS ==================== --}}
+@section('scripts')
+@endsection
