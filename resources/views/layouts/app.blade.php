4eef361b7bf2facbd35564db54f3b7bd04a43b00
ikhlas-ct <ikhlasulamalmawardi@gmail.com>
2025-10-29 04:23:22 +0700

    perapian kode sebelumnya

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="{{ asset('user/css/vertical-layout-light/style.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+        
 
         @yield('styles')
     </head>
