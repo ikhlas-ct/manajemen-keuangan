c25ae06ee7c881d8e94fcfb4583acb7dc52270d7
aliyah <137191184+MysticGazer@users.noreply.github.com>
2025-10-28 15:59:17 +0700

    selesai crud kategori dan fe (#19)

--- resources/views/pages/admin/categorie/modal-create.blade.php
+++ resources/views/pages/admin/categorie/modal-create.blade.php
@@ -21,7 +21,7 @@
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
           <button type="submit" class="btn btn-primary">Simpan</button>
         </div>
       </form>
